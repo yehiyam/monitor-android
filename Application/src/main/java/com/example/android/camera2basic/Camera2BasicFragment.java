97c6f66b5bb420285ae06b206d914112e0362fb0
neil <nilnil47@gmail.com>
2020-03-23 22:50:12 +0200

    working scheduled taking pictures by handler

--- Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
@@ -72,7 +72,7 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class Camera2BasicFragment extends Fragment
-        implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     /**
      * Conversion from screen rotation to JPEG orientation.
@@ -431,8 +431,6 @@ public class Camera2BasicFragment extends Fragment
 
     @Override
     public void onViewCreated(final View view, Bundle savedInstanceState) {
-        view.findViewById(R.id.picture).setOnClickListener(this);
-        view.findViewById(R.id.info).setOnClickListener(this);
         mTextureView = (AutoFitTextureView) view.findViewById(R.id.texture);
     }
 
@@ -889,25 +887,25 @@ public class Camera2BasicFragment extends Fragment
         }
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.picture: {
-                takePicture();
-                break;
-            }
-            case R.id.info: {
-                Activity activity = getActivity();
-                if (null != activity) {
-                    new AlertDialog.Builder(activity)
-                            .setMessage(R.string.intro_message)
-                            .setPositiveButton(android.R.string.ok, null)
-                            .show();
-                }
-                break;
-            }
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.picture: {
+//                takePicture();
+//                break;
+//            }
+//            case R.id.info: {
+//                Activity activity = getActivity();
+//                if (null != activity) {
+//                    new AlertDialog.Builder(activity)
+//                            .setMessage(R.string.intro_message)
+//                            .setPositiveButton(android.R.string.ok, null)
+//                            .show();
+//                }
+//                break;
+//            }
+//        }
+//    }
 
     private void setAutoFlash(CaptureRequest.Builder requestBuilder) {
         if (mFlashSupported) {
--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -17,20 +17,78 @@
 package com.example.android.camera2basic;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CameraActivity extends AppCompatActivity {
+
+    public final int DELAY_BEFORE_TAKING_PICTURES_MILLIS = 5000;
+    public final int IMAGE_FREQUENCY_MILI = 3000;
+
+    Handler handler;
+
+    Runnable takingPicturesRunnable;
+
+    private Camera2BasicFragment camera2BasicFragment;
+
+    Button stopTakingPicturesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-//        PictureCaptureService.startActionTakingPictures(getApplicationContext());
+        camera2BasicFragment = Camera2BasicFragment.newInstance();
+         stopTakingPicturesButton = findViewById(R.id.stop_taking_pictures_button);
+
         if (null == savedInstanceState) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.camera_container, Camera2BasicFragment.newInstance())
+                    .replace(R.id.camera_container, camera2BasicFragment)
                     .commit();
         }
+
+        handler = new Handler();
+        takingPicturesRunnable = new Runnable() {
+            @Override
+            public void run() {
+                camera2BasicFragment.takePicture();
+                handler.postDelayed(takingPicturesRunnable, IMAGE_FREQUENCY_MILI);
+            }
+        };
+
+        stopTakingPicturesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopTakingPictures();
+            }
+        });
+
+        startTakingPictures();
     }
+
+    public void startTakingPictures() {
+        handler.postDelayed(takingPicturesRunnable, DELAY_BEFORE_TAKING_PICTURES_MILLIS);
+    }
+
+    public void stopTakingPictures() {
+        handler.removeCallbacks(takingPicturesRunnable);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopTakingPictures();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startTakingPictures();
+    }
+
+    //    AlertDialog alertDialog =
 
 }
